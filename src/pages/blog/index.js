d9252dbfa7fdec5009334843a280c230d4e5700d
do luong <doluong2007@gmail.com>
2022-11-17 09:32:24 +0700

    finish part 6

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,25 +1,27 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
-import Layout from '../components/layout'
-import Seo from '../components/seo'
+import { Link, graphql } from 'gatsby'
+import Layout from '../../components/layout'
+import Seo from '../../components/seo'
 
 const BlogPage = ({ data }) => {
-    return (
-      <Layout pageTitle="My Blog Posts">
-              {
-        data.allMdx.nodes.map((node) => (
+  return (
+    <Layout pageTitle="My Blog Posts">
+      {
+        data.allMdx.nodes.map(node => (
           <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2>
+            <h2>
+              <Link to={`/blog/${node.frontmatter.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
             <p>Posted: {node.frontmatter.date}</p>
-            <p>{node.excerpt}</p>
           </article>
         ))
       }
+    </Layout>
+  )
+}
 
-      </Layout>
-    )
-  }
-  
 export const query = graphql`
   query {
     allMdx(sort: { frontmatter: { date: DESC }}) {
@@ -27,14 +29,13 @@ export const query = graphql`
         frontmatter {
           date(formatString: "MMMM D, YYYY")
           title
+          slug
         }
         id
-        excerpt
       }
     }
   }
-`  
-
+`
 
 export const Head = () => <Seo title="My Blog Posts" />
 
